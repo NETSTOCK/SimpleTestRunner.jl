5bb61e511f5cf705736e5e86fd538a587ea1f504
Sheldon Hearn <sheldon.hearn@netstock.co>
2023-08-07 11:09:09 +0200

    Add script to exercise various workflows

---
+++ various-tests.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -ex
+#
+# A collection of invocations that has been helpful in protecting the various
+# workflows.
+
+# Running all the tests in various ways
+
+julia --project=. test/runtests.jl
+julia --project=. -e 'using Pkg; Pkg.test()'
+julia --project=. -e 'include("test/runtests.jl");'
+
+cd test
+julia --project=.. runtests.jl
+cd ..
+julia --project=. -e 'cd("test"); include("setup.jl"); include("runtests.jl");'
+
+
+# Running specific test files in various ways
+
+julia --project=. test/runtests.jl runtests test_names
+julia --project=. -e 'include("test/setup.jl"); include("test/runtests_tests.jl");'
+julia --project=. -e 'include("test/setup.jl"); include("test/test_names_tests.jl");'
